cd0e2ff769f87ef012ca7f551c434f99a0e61b1b
Mitresh Lukhi <mitresh.22397@gmail.com>
2023-01-17 16:41:04 +0000

    7

---
+++ Patterns/InvertedzeroonePyramid.java
@@ -0,0 +1,19 @@
+package C.Patterns;
+
+public class InvertedzeroonePyramid {
+    public static void main(String[] args) {
+        int n=5;
+
+        for (int i =1; i<=5; i++){
+            for (int j =1; j<=n-i+1; j++){
+                int sum= i+j;
+                if (sum%2==0){
+                    System.out.print("1 ");
+                }else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
